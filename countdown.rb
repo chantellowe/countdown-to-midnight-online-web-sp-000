622491c76e8ca1f89a4dd870309139c3c8c078fe
Chantel Lowe <chantellowe17@gmail.com>
2019-07-15 05:55:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -15,14 +15,13 @@ end
 
 
 def countdown_with_sleep(number)
-  sleep(1)
     number = 10
     
     while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
+    sleep(1)
     end
-  end
   
   "HAPPY NEW YEAR!"
   
